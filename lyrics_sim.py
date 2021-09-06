c0a8912f192aca09c6803513e4a7e7dcd0f20811
roccotrip <rocco.trip@gmail.com>
2021-09-06 16:58:14 +0200

    first commit

---
+++ lyrics_sim.py
@@ -0,0 +1,60 @@
+import argparse
+import os, pickle
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+def get_matrices(input, folder1, lyrics1, folder2, lyrics2):
+    in_file = os.path.join(input, folder1, lyrics1 + '.pkl')
+    with open(in_file, 'rb') as fr:
+        song1_emb = pickle.load(fr)
+    in_file = os.path.join(input, folder2, lyrics2 + '.pkl')
+    with open(in_file, 'rb') as fr:
+        song2_emb = pickle.load(fr)
+    return song1_emb, song2_emb
+
+
+def overall_sim(input, folder1, folder2, lyrics1, lyrics2):
+    song1_emb, song2_emb = get_matrices(input, folder1, lyrics1, folder2, lyrics2)
+    matrix1 = np.array([song1_emb[k]['embedding'].tolist() for k in song1_emb.keys()])
+    matrix2 = np.array([song2_emb[k]['embedding'].tolist() for k in song2_emb.keys()])
+    vector1 = np.mean(matrix1, axis=0)
+    vector2 = np.mean(matrix2, axis=0)
+    sim = cosine_similarity(np.array([vector1.tolist(), vector2.tolist()]))[0,1]
+    print('The overall similarity between the two lyrics is {}'.format(sim))
+
+def sim(input, folder1, folder2, lyrics1, lyrics2, min_similarity):
+    song1_emb, song2_emb = get_matrices(input, folder1, lyrics1, folder2, lyrics2)
+    matrix1 = np.array([song1_emb[k]['embedding'].tolist() for k in song1_emb.keys()])
+    matrix2 = np.array([song2_emb[k]['embedding'].tolist() for k in song2_emb.keys()])
+    sim_mat = cosine_similarity(matrix1, matrix2)
+    similarities = np.where(sim_mat > min_similarity)
+    print('Found {} similar lines:'.format(len(similarities[0])))
+    for x, y in zip(similarities[0],similarities[1]):
+        print(sim_mat[x, y])
+        print(song1_emb[x]['line'])
+        print(song2_emb[y]['line'])
+        print('')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str, default='embeddings')
+    parser.add_argument('--folder1', type=str)
+    parser.add_argument('--folder2', type=str)
+    parser.add_argument('--lyrics1', type=str)
+    parser.add_argument('--lyrics2', type=str)
+    parser.add_argument('--min_similarity', type=float, default=.75)
+    parser.add_argument('--sim_type', type=str, default='sentence')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    if args.sim_type == 'sentence':
+        sim(args.input, args.folder1, args.folder2, args.lyrics1, args.lyrics2, args.min_similarity)
+    if args.sim_type == 'overall':
+        overall_sim(args.input, args.folder1, args.folder2, args.lyrics1, args.lyrics2)
+
+
+# Queen__Breakthru
+# Queen__A Kind Of Magic
--- main.py
+++ main.py
@@ -1,3 +1,51 @@
-import re
-import tqdm
-import random
+import argparse
+import os, csv, tqdm, pickle, re
+from laserembeddings import Laser
+
+def embedder(lines):
+    model = Laser()
+    return model.embed_sentences(lines, lang='en')
+
+
+def clean_lines(lyrics, min_line_len):
+    lines = lyrics.split('\n')
+    lines = [re.sub(' +', ' ', line).strip() for line in lines]
+    lines = [line for line in lines if
+             len(line) > min_line_len and
+             line.startswith(('Lyrics licensed', 'Publisher: ', 'Writer/s: ')) == False]
+    return lines
+
+def embed(input_folder, output_folder, files, min_line_len):
+    for fname in files:
+        out_path = os.path.join(output_folder, fname)
+        if not os.path.exists(out_path):
+            os.makedirs(out_path)
+        with open(os.path.join(input_folder, 'lyrics-' + fname +'.csv')) as f:
+            reader = csv.reader(f, delimiter=',')
+            lyrics = [row for row in reader]
+        for song in tqdm.tqdm(lyrics[1:]):
+            file_, artist, title, lyrics = song
+            out_file = os.path.join(out_path, '__'.join([artist, title])+'.pkl')
+            if not os.path.exists(out_file):
+                lines = clean_lines(lyrics, min_line_len)
+                embeddings = embedder(lines)
+                song_emb = {}
+                for i, line in enumerate(lines):
+                    song_emb.setdefault(i, {})
+                    song_emb[i]['line'] = line
+                    song_emb[i]['embedding'] = embeddings[i]
+                with open(out_file, 'wb') as fw:
+                    pickle.dump(song_emb, fw)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str, default='lyrics')
+    parser.add_argument('--output', type=str, default='embeddings')
+    parser.add_argument('--files', type=list, default=['songfacts', 'genius'])
+    parser.add_argument('--min_line_len', type=int, default=10)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    embed(args.input, args.output, args.files, args.min_line_len)
